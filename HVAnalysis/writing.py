566f112bfd0f42755ac37a3c263fc8daf85f16ac
Ernest Sorokun <sorokun.ernest@gmail.com>
2022-08-02 21:14:58 +0300

    edit b<b1 original code part

--- HVAnalysis/writing.py
+++ HVAnalysis/writing.py
@@ -22,9 +22,9 @@ class Writer:
                 b = row.Index
                 r = row.resistance
                 vps = row.avgvolt
-                '''
+
                 if b <= b1:
-                    if streamerON and 1472 < r or r < 1452 or vps < 120000.:
+                    if not streamerON and 1472 < r or r < 1452 or vps < 120000.:
                         streamerON = True
                         startStream = b
                     elif 1452 < r < 1472 and vps > 120000. and streamerON:
@@ -32,16 +32,7 @@ class Writer:
                         cutONperiod.append([startStream - timedelta(0, 2), b + timedelta(0, 2)])
                         writer.writerow([int(pytime.mktime((startStream - timedelta(0, 2)).timetuple())),
                                      int(pytime.mktime((b + timedelta(0, 2)).timetuple()))])
-                '''
-                if b <= b1 and (r > 1472 or r < 1452 or vps < 120000.) and not streamerON:
-                    streamerON = True
-                    startStream = b
-                if b <= b1 and (r > 1452 and r < 1472 and vps > 120000.) and streamerON:
-                    streamerON = False
-                    cutONperiod.append([startStream - timedelta(0, 2), b + timedelta(0, 2)])
-                    writer.writerow([int(pytime.mktime((startStream - timedelta(0, 2)).timetuple())),
-                                     int(pytime.mktime((b + timedelta(0, 2)).timetuple()))])
-
+                        
                 if b > b1 and b < b2 and (r < 1465 or vps < 120000.) and not streamerON:
                     streamerON = True
                     startStream = b
