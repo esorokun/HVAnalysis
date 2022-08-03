4fc7654d5cd31792008e1b8dd26a045a113c327d
Ernest Sorokun <sorokun.ernest@gmail.com>
2022-08-03 10:32:56 +0300

    re-name in writing

--- HVAnalysis/plotting.py
+++ HVAnalysis/plotting.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import conf
-import numpy as np
 import pandas as pd
 
 
--- HVAnalysis/writing.py
+++ HVAnalysis/writing.py
@@ -1,8 +1,6 @@
 import csv
-import pandas as pd
 from datetime import datetime, timedelta
-import time as pytime
-import logging
+
 
 class Writer:
     def __init__(self, df_wrapper):
@@ -13,19 +11,21 @@ class Writer:
         b2 = datetime(2018, 10, 17, 12, 0, 0)
         df = self.df_wrapper.data_frame
         stream = False
-        cutONperiod = []
+        cut_time = []
 
-        def period_cut_writer(unstablelist, file, start, end):
+        def period_cut_writer(unstable_list, file, start, end):
             i = start - timedelta(0, 2)
             while i < end + timedelta(0, 2):
-                unstablelist.append([i])
+                unstable_list.append([i])
                 file.writerow([i])
                 i += timedelta(0, 1)
 
         with open(file_name, mode='w') as f:
             writer = csv.writer(f, delimiter=',')
             for row in df.itertuples():
-                if row.ncurr == 0 or row.nvolt == 0: continue # piecewise cut on resistance
+                if row.ncurr == 0 or row.nvolt == 0:
+                    continue
+                # piecewise cut on resistance
                 b = row.Index
                 r = row.resistance
                 vps = row.avgvolt
@@ -33,29 +33,31 @@ class Writer:
                 if b <= b1:
                     if not stream and 1472 < r or r < 1452 or vps < 120000.:
                         stream = True
-                        startStream = b
+                        start_stream = b
                     elif 1452 < r < 1472 and vps > 120000. and stream:
                         stream = False
-                        period_cut_writer(cutONperiod, writer, startStream, b)
+                        period_cut_writer(cut_time, writer, start_stream, b)
                 if b1 < b < b2:
                     if not stream and (r < 1465 or vps < 120000.):
                         stream = True
-                        startStream = b
+                        start_stream = b
                     elif stream and (r > 1465 and vps > 120000.) and stream:
                         stream = False
-                        period_cut_writer(cutONperiod, writer, startStream, b)
+                        period_cut_writer(cut_time, writer, start_stream, b)
                 if b >= b2:
                     if not stream and (r < 1465 or vps < 180000.):
                         stream = True
-                        startStream = b
+                        start_stream = b
                     elif stream and (r > 1465 and vps > 180000.) and stream:
                         stream = False
-                        period_cut_writer(cutONperiod, writer, startStream, b)
+                        period_cut_writer(cut_time, writer, start_stream, b)
+
+
 '''
     def create_unstable_date_df(self, file_name):
         self.write_streamer_periods(file_name)
         df = pd.read_csv(file_name,
-                         index_col=0, usecols=[0], names=['timestamp'])
+                         index_col=0, use_cols=[0], names=['timestamp'])
         df.index = 1000000000 * df.index
         df['timestamp'] = pd.to_datetime(df.index)
         logging.info(f'HeinzWrapper.stable_data_frame =\n{df}')
