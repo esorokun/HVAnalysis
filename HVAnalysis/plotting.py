44e4152fbdd1c155beeeb36af1cf136816a64a57
Ernest Sorokun <sorokun.ernest@gmail.com>
2022-08-03 14:26:38 +0300

    change way of building plots from class --> data_frame

--- HVAnalysis/plotting.py
+++ HVAnalysis/plotting.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import conf
 import pandas as pd
+import logging
 
 
 class Plotter:
     def __init__(self, df_wrapper_1, df_wrapper_2=None):
         if df_wrapper_2 is not None:
-            self.df_wrapper = pd.merge(df_wrapper_1, df_wrapper_2, on='timestamp')
+            self.df_wrapper = pd.merge(df_wrapper_1.data_frame, df_wrapper_2.data_frame, on='timestamp')
+            logging.info(f'HeinzWrapper.data_frame =\n{self.df_wrapper}')
         else:
-            self.df_wrapper = df_wrapper_1
+            self.df_wrapper = df_wrapper_1.data_frame
 
     def plot(self, saver):
         self.df_wrapper.plot(y=['resistance', 'avgcurr', 'avgvolt'])
@@ -32,7 +34,7 @@ class Plotter:
         if savename is not None: plt.savefig(f'{conf.output_folder}/{savename}.png')
 
     def plot_scatter(self, name_x='avgcurr', name_y='avgvolt', savename=None):
-        _ = plt.scatter(y=self.df_wrapper([name_y]), x=self.df_wrapper([name_x]), alpha=0.05, s=0.1)
+        self.df_wrapper.plot.scatter(y=[name_y], x=[name_x], alpha=0.05, s=0.1)
         plt.xlabel(name_x)
         plt.ylabel(name_y)
         plt.show()
