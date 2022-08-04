40dc8879695bd127b6e19fa860c162cacad016e0
Ernest Sorokun <sorokun.ernest@gmail.com>
2022-08-04 22:57:55 +0300

    edit b1 func for normal df

--- HVAnalysis/new_writing.py
+++ HVAnalysis/new_writing.py
@@ -83,40 +83,81 @@ class NewWriter:
                  #   self.last_period_cut_writer(cut_time, writer, start_stream, b)
 
     def create_unstable_df_for_b1(self, df):
-        model_1 = (1452 > df['resistance'] or df['resistance'] > 1472 and df['avgvolt'] < 12000.)
-        df_res = df[model_1]
+
+        model_1 = df['avgvolt'] < 120000.
+        df_1 = df[model_1]
+
+        model_2 = 1452 > df['resistance']
+        df_2 = df[model_2]
+
+        model_3 = df['resistance'] > 1472
+        df_3 = df[model_3]
+
+        df_res = pd.concat([df_1, df_2, df_3]).drop_duplicates()
+        df_res = df_res.sort_index()
         return df_res
 
-    def create_unstable_df_for_b1_b2(self, df, df_add):
-        model_1 = (df['resistance'] < 1465 and df['avgvolt'] < 12000.)
-        df_res = df[model_1]
-        result = pd.concat([df_add, df_res])
+    def create_unstable_df_for_b1_b2(self, df):
+        b1 = datetime(2018, 10, 5, 0, 0, 0)
+        map = df.index <= b1
+        df_b1, df_b1_b2 = df[map], df[~map]
+        model = df_b1_b2['resistance'] < 1465
+        df_b1_b2_ver1 = df_b1_b2[model]
+        model_2 = df_b1_b2_ver1['avgvolt'] < 120000.
+        df_res_2 = df_b1_b2_ver1[model_2]
+        df_res_1 = self.create_unstable_df_for_b1(df_b1)
+        result = pd.concat([df_res_1, df_res_2])
         return result
 
-    def create_unstable_df_for_b2(self, df, df_add):
-        model_1 = (df['resistance'] < 1465 and df['avgvolt'] < 18000.)
-        df_res = df[model_1]
-        result = pd.concat([df_add, df_res])
+    def create_unstable_df_for_b2(self, df):
+        b2 = datetime(2018, 10, 17, 12, 0, 0)
+        map = df.index < b2
+        df_b1_b2, df_b2 = df[map], df[~map]
+        model = df_b2['resistance'] < 1465
+        df_b2_ver1 = df_b2[model]
+        model_2 = df_b2_ver1['avgvolt'] < 180000.
+        df_res_2 = df_b2_ver1[model_2]
+        df_res_1 = self.create_unstable_df_for_b1_b2(df_b1_b2)
+        result = pd.concat([df_res_1, df_res_2])
         return result
 
-    def new_write_stremer_periods(self):
+    def new_df_unstable_periods(self):
+
         b1 = datetime(2018, 10, 5, 0, 0, 0)  # 2018-10-05 00:00:00
         b2 = datetime(2018, 10, 17, 12, 0, 0)  # 2018-10-17 12:00:00
         df = self.df_wrapper.data_frame
         last_index = df.last_valid_index()
-        if last_index > b2:
-
-            map_1 = df.index <= b1
-            df_b1, df_2 = df[map_1], df[~map_1]  #df for  --> b1 and b1-->
-            map_2 = df_2.index < b2
-            df_b1_b2, df_b2 = df_2[map_1], df_2[~map_1]  #separate  b1--> on b1-->b2 and b2-->
-
-        elif last_index < b2:
-            map_1 = df.index <= b1
-            df_b1, df_b1_b2 = df[map_1], df[~map_1]
-
-
+        df_unstable = []
+        if last_index >= b2:
+            print(1)
+            df_unstable = self.create_unstable_df_for_b2(df)
+        elif last_index > b1:
+            print(2)
+            df_unstable = self.create_unstable_df_for_b1_b2(df)
         elif last_index <= b1:
+            print(3)
+            df_unstable = self.create_unstable_df_for_b1(df)
+
+        #logging.info(f'HeinzWrapper.data_frame =\n{df_unstable}')
+        return df_unstable
+
+    def new_df_unstable_writer(self):
+        df = self.new_df_unstable_periods()
+        with open(self.file_name, mode='w') as f:
+            writer = csv.writer(f)
+            start = list[0]
+            c = start - timedelta(0, 1)
+            for row in df.itertuples():
+                if row.ncurr == 0 or row.nvolt == 0:
+                    continue
+                b = row.Index
+                if int(b-1) == c:
+                    writer.writerow([int(pytime.mktime((start - timedelta(0, 2)).timetuple())),
+                                     int(pytime.mktime((b + timedelta(0, 2)).timetuple()))])
+                    start = int(b)
+                c = int(b)
+
+
 
 
 
