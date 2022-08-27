2a46d2f32a33ed64686709f47728c4a165724916
Ernest Sorokun <sorokun.ernest@gmail.com>
2022-08-27 10:25:29 +0300

    add ML class

---
+++ HVAnalysis/ML.py
@@ -0,0 +1,33 @@
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pandas as pd
+
+class MLDataFrame:
+    def __init__(self, data_frame):
+        self.data_frame = data_frame
+        self.curr_volt_res_df = NotImplemented
+        self.log10_volt_curr_df = NotImplemented
+
+
+    def transform_data(self):
+        df = self.data_frame
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        df[['binavgcurr', 'binavgvolt', 'binresistance']] = scaler.fit_transform(
+            df[['avgcurr', 'avgvolt', 'resistance']])
+        df['binavgcurr'] = df['binavgcurr'].values.reshape(-1, 1)
+        df['binavgvolt'] = df['binavgvolt'].values.reshape(-1, 1)
+        df['binresistance'] = df['binresistance'].values.reshape(-1, 1)
+        df_learn = pd.DataFrame({'binavgcurr': df['binavgcurr'], 'binavgvolt': df['binavgvolt'],
+                                 'binresistance': df['binresistance']})
+        self.curr_volt_res_df = df_learn
+
+    def add_log10_params(self):
+        df = self.data_frame
+        df['logcurr'] = np.log10(df['avgcurr'])
+        df['logvolt'] = np.log10(df['avgvolt'])
+        df['logcurr'] = df['logcurr'].ffill(axis=0)
+        df['logvolt'] = df['logvolt'].ffill(axis=0)
+        df_learn = pd.DataFrame({'logcurr': df['logcurr'], 'logvolt': df['logvolt'],
+                                 'avgcurr': df['avgcurr'], 'avgvolt': df['avgvolt'],
+                                 'resistance': df['resistance']})
+        self.log10_volt_curr_df = df_learn
