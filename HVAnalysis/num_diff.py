9853eccf0b1e2401d2dd745adb398cfbae6c0d30
Ernest Sorokun <sorokun.ernest@gmail.com>
2022-09-04 14:39:03 +0300

    mean working algorithm

--- HVAnalysis/num_diff.py
+++ HVAnalysis/num_diff.py
@@ -44,13 +44,54 @@ def first(df):
     plt.show()
 
 def second(df):
+    df_res = df.copy()
     new_df = df.copy()
     new_df['datetime'] = new_df.index
     new_df = new_df.reset_index()
     new_df['num'] = new_df.index
+
     new_df = new_df.loc[new_df['num'] % 180 == 0]
     new_df = new_df.drop([0])
+    datelist = new_df['datetime']
     print(new_df)
+
+    new_df = new_df.set_index('datetime')
+    df = df.join(new_df[['num']])
+    df = df.ffill(axis=0)
+    df = df.bfill(axis=0)
+
+    df['datetime'] = df.index
+    new_df = df.groupby(['num']).mean()
+    new_df = new_df.join(datelist)
+
+    new_df['checker'] = np.abs((new_df['avgcurr'].shift(-1) - new_df['avgcurr'].shift(1)) / 360)
+    new_df = new_df.set_index('datetime')
+    df = df_res
+    df = df.join(new_df[['checker']])
+    df = df.ffill(axis=0)
+    df = df.bfill(axis=0)
+    print(df)
+    df.loc[np.abs(df['checker']) < 0.001, 'result_curr'] = 0
+    df.loc[df['result_curr'] != 0, 'result_curr'] = 1
+
+    df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(-1) * 1.05), 'result_curr'] = 1
+    df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(1) * 1.05), 'result_curr'] = 1
+
+    print(df['result_curr'].sum())
+    print(df['result_curr'])
+
+    # df['result'] = df['result_volt']
+    df.loc[df['result_curr'] == 1, 'result'] = 1
+    df.loc[df['result'] != 1, 'result'] = 0
+    df['datetime'] = df.index
+    length = int(len(df))
+    unstable = int(df['result_curr'].sum())
+    perc = np.round(unstable / length, 2)
+    print(str(perc) + "%\n" + str(100 - perc) + "%")
+
+    df['datetime'] = df.index
+    sns.scatterplot(x='datetime', y='avgcurr', data=df, alpha=1, s=5, hue='result')
+    plt.show()
 
 def main(args):
     conf.configure_from_args(args)
