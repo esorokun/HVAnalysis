d51c21ff2156bec88a28dc45a3cf1593097a02b4
Ernest Sorokun <sorokun.ernest@gmail.com>
2022-09-04 19:29:38 +0300

    Final version of num diff

--- HVAnalysis/num_diff.py
+++ HVAnalysis/num_diff.py
@@ -50,7 +50,7 @@ def second(df):
     new_df = new_df.reset_index()
     new_df['num'] = new_df.index
 
-    new_df = new_df.loc[new_df['num'] % 100 == 0]
+    new_df = new_df.loc[new_df['num'] % 60 == 0]
     new_df = new_df.drop([0])
     datelist = new_df['datetime']
 
@@ -60,18 +60,40 @@ def second(df):
     df = df.bfill(axis=0)
 
     df['datetime'] = df.index
+    '''
     df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(-1) * 1.05)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(-1) * 0.95)]
     df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(1) * 1.05)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(1) * 0.95)]
     df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(-10) * 1.05)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(-10) * 0.95)]
     df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(10) * 1.05)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(10) * 0.95)]
     df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(-20) * 1.05)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(-20) * 0.95)]
     df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(20) * 1.05)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(20) * 0.95)]
     df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(-30) * 1.05)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(-30) * 0.95)]
     df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(30) * 1.05)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(30) * 0.95)]
+    '''
+    df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(1) * 1.05)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(1) * 0.95)]
+    df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(-1) * 1.05)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(-1) * 0.95)]
+    df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(120) * 1.05)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(120) * 0.95)]
+    df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(-120) * 1.05)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(-120) * 0.95)]
     new_df = df.groupby(['num']).mean()
     new_df = new_df.join(datelist)
 
-    new_df['checker'] = np.abs((new_df['avgcurr'].shift(-1) - new_df['avgcurr'].shift(1)) / 200)
+    new_df['checker_left'] = np.abs((new_df['avgcurr'].shift(-1) - new_df['avgcurr']) / 60)
+    new_df['checker_right'] = np.abs((new_df['avgcurr'].shift(1) - new_df['avgcurr']) / 60)
+    new_df['checker_sides'] = np.abs((new_df['avgcurr'].shift(-1) - new_df['avgcurr'].shift(1)) / 120)
+    new_df['checker'] = \
+        (np.abs(new_df['checker_left']) + np.abs(new_df['checker_right']))/2
     new_df = new_df.set_index('datetime')
     df = df_res
     df = df.join(new_df[['checker']])
@@ -107,6 +129,63 @@ def second(df):
     df['datetime'] = df.index
     sns.scatterplot(x='datetime', y='avgcurr', data=df, alpha=1, s=5, hue='result')
     plt.show()
+
+
+def third(df):
+    df_res = df.copy()
+    new_df = df.copy()
+    new_df['datetime'] = new_df.index
+    new_df = new_df.reset_index()
+    new_df['num'] = new_df.index
+
+    new_df = new_df.loc[new_df['num'] % 600 == 0]
+    new_df = new_df.drop([0])
+    datelist = new_df['datetime']
+
+    new_df = new_df.set_index('datetime')
+    df = df.join(new_df[['num']])
+    df = df.ffill(axis=0)
+    df = df.bfill(axis=0)
+
+    df['datetime'] = df.index
+
+    df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(1) * 1.2)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(1) * 0.8)]
+    df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(-1) * 1.2)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(-1) * 0.8)]
+    df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(60) * 1.2)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(60) * 0.8)]
+    df = df.loc[np.abs(df['avgcurr']) < np.abs(df['avgcurr'].shift(-60) * 1.2)]
+    df = df.loc[np.abs(df['avgcurr']) > np.abs(df['avgcurr'].shift(-60) * 0.8)]
+    new_df = df.groupby(['num']).mean()
+    new_df = new_df.join(datelist)
+
+    new_df['checker'] = np.abs((new_df['avgcurr'].shift(-1) - new_df['avgcurr'].shift(1)) / 1200)
+    new_df = new_df.set_index('datetime')
+    df = df_res
+    df = df.join(new_df[['checker']])
+    new_df = new_df.rename(columns={"avgcurr": "meancurr"})
+    df = df.join(new_df[['meancurr']])
+    df = df.ffill(axis=0)
+    df = df.bfill(axis=0)
+    print(df)
+    df.loc[np.abs(df['checker']) < 0.01, 'result_curr'] = 0
+    df.loc[df['result_curr'] != 0, 'result_curr'] = 1
+    df.loc[np.abs(df['avgcurr']) > np.abs(df['meancurr'] * 1.025), 'result_curr'] = 1
+    df.loc[np.abs(df['avgcurr']) < np.abs(df['meancurr'] * 0.975), 'result_curr'] = 1
+
+    df.loc[df['result_curr'] == 1, 'result'] = 1
+    df.loc[df['result'] != 1, 'result'] = 0
+    df['datetime'] = df.index
+    length = int(len(df))
+    unstable = int(df['result_curr'].sum())
+    perc = np.round(unstable / length, 2)
+    print(str(perc) + "%\n" + str(100 - perc) + "%")
+
+    df['datetime'] = df.index
+    sns.scatterplot(x='datetime', y='avgcurr', data=df, alpha=1, s=5, hue='result')
+    plt.show()
+
 
 def main(args):
     conf.configure_from_args(args)
