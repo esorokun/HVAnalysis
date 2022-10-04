df70519da1f4a60376487b29ed82e3739ade4776
Ernest Sorokun <sorokun.ernest@gmail.com>
2022-10-04 16:56:48 +0300

    make timedelta for full data

--- HVAnalysis/start_main.py
+++ HVAnalysis/start_main.py
@@ -26,33 +26,17 @@ def main(args):
 
     conf.configure_from_args(args)
     curr_wrapper = HeinzWrapper(conf.curr_file_names, 'curr')
-    c = curr_wrapper.resample_count('S')
-    df1 = curr_wrapper.resample_value('S')
-    df1 = df1.join(c)
-    df1 = df1.loc[df1['ncurr'] != 0]
-    df1['avgcurr'] = df1['sumcurr'] / df1['ncurr']
-    df1.loc[df1.index, 'datetime'] = df1.index
-
-    wrapper = HeinzWrapper(conf.GroundPlanes_Ch14, 'curr')
-    c = wrapper.resample_count('S')
-    df = wrapper.resample_value('S')
-    df = df.join(c)
-    df['avgcurr'] = df['sumcurr']/df['ncurr']
-    df = df.loc[df['ncurr'] != 0]
-    df = df.loc[np.abs(df['avgcurr']) > 3.7]
-    df.loc[df.index, 'datetime'] = df.index
-
-    df_o = df1.join(df, lsuffix='_original', rsuffix='_called')
-    df_o.loc[df_o['datetime_called'] == df_o['datetime_original'], 'result'] = 1
-    df_o.loc[df_o['datetime_called'] != df_o['datetime_original'], 'result'] = 0
-    #df1.loc[df['datetime'] == df1['datetime'], 'result'] = 1
+    volt_wrapper = HeinzWrapper(conf.volt_file_names, 'volt')
+    wrapper = ResistanceWrapper(volt_wrapper, curr_wrapper)
+    df = wrapper.data_frame
+    df = df.loc[(df['nvolt'] != 0) & (df['ncurr'] != 0)]
+    df['datetime'] = (df.index.astype('uint64') / 1_000_000_000).astype(np.int64)
+    df['datetime_next'] = df['datetime'].shift(+1)
+    df = df.bfill(axis=0)
+    df['timedelta'] = df['datetime'] - df['datetime_next']
     print(df)
-    print(df1)
-    print(df_o)
-    # df_curr.loc[df_curr['datetime'] == df['datetime'], 'result'] = 'unstable'
-    # df_curr.loc[df_curr['result'] != 'unstable', 'result'] = 'stable'
-    sns.scatterplot(data=df_o, x='datetime_original', y='avgcurr_original', alpha=1, s=5, hue='result')
-    plt.show()
+    #sns.scatterplot(data=df_o, x='datetime_original', y='avgcurr_original', alpha=1, s=5, hue='result')
+    #plt.show()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
